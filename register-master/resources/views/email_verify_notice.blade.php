a2f03eaa4e60ec6857a58faaf1e5ead8623236a1
Antoxa <datskiiak@yandex.ru>
2023-11-03 01:00:55 +0300

    add invisible home page for not verified users

---
+++ register-master/resources/views/email_verify_notice.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('content')
+    <p>Email must be verified</p>
+@endsection('content')
--- register-master/routes/web.php
+++ register-master/routes/web.php
@@ -7,12 +7,13 @@ Route::get('/register', "RegisterController@create")->middleware('guest')->name(
 Route::post('/register', "RegisterController@store")->middleware('guest')->name('register.store');
 
 Route::view('/main', "main")->name('main');
-Route::view('/home', 'home')->middleware('auth')->name('home');
+Route::view('/home', 'home')->middleware(['auth', 'verified'])->name('home');
 Route::view('/login', 'login')->middleware('guest')->name('login');
 
 Route::get('/email/verify', fn()=>view('email_verify'))->middleware('auth')->name('verify');
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
-    return redirect()->intended(route('verify'));
+    return redirect()->route('verify');
 })->middleware(['auth', 'signed'])->name('verification.verify');
+Route::get('/email/verify/notice', fn()=>view('email_verify_notice'))->middleware('auth')->name('verification.notice');
 
