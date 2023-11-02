883fc28a3e3ba30939c628088566d0ccf820523f
Antoxa <datskiiak@yandex.ru>
2023-11-02 20:59:10 +0300

    add exception handling during validation

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -33,7 +33,28 @@ class RegisterController extends Controller
             'authors' => 'exists:authors,id',
             'categories' => 'exists:categories,id'
         ],[
-            'login.unique' => 'Пользователь с таким email уже существует!',     // пользовательские ошибки
+            'login.required' => 'Введите логин!',
+            'login.unique' => 'Пользователь с таким логином уже существует!',     // пользовательские ошибки
+
+            'email.required' => 'Введите mail!',
+            'email.unique' => 'Пользователь с таким email уже существует',
+            'email.email' => 'Введите корректный email',
+
+            'password.required' => 'Введите пароль!',
+            'password.min:8' => 'Пароль должен содержать минимум 8 символов',
+            'password.confirmed' => 'Пароли не совпадают!',
+
+            'name.string' => 'Имя должно быть строкой',
+            'secondary_name.string' => 'Фамилия должна быть строкой',
+            'birthday.date' => 'День рождения должен быть датой',
+            'phone_number.unique' => 'Пользователь с таким номером телефона уже существует!',
+            'phone_number.regex:/(7)[0-9]{10}/' => 'Телефон должен соответсвовать виду: 7ХХХХХХХХХХ',
+            'about.string' => 'Должно быть строкой',
+            'photo.image' => 'Загржуено не фото',
+            'photo.mimes:jpeg,jpg,png,svg' => 'Поддерживаемые расширения файлов: jpeg,jpg,png,svg',
+            'photo.max:2048' => 'Размер фото превышает 2 Мб',
+            'authors.exists:authors,id' => 'Автор не существует',
+            'categories.exists:categories,id' => 'Категория не существует',
         ]);
 
         if($request->hasFile('photo')) {
